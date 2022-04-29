39816fdcc0c5c1d2e8b61ec3d91252b6ca85e390
Olakunle Balogun <olakunlebalogun247@gmail.com>
2022-04-29 11:40:31 +0100

    added and completed the footer

--- Todo-App/src/components/refactored/Footer/Footer.js
+++ Todo-App/src/components/refactored/Footer/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Footer.scss";
+import { IconContext } from "react-icons";
 import {
 	FaFacebookF,
 	FaGithub,
@@ -14,19 +16,31 @@ const Footer = () => {
 			<div className="container">
 				<div className="foot d-flex justify-content-around">
 					<div className="first">
-						<p>
-							%40 -- OlakunleBalo -- Being and Building Software <br />{" "}
+						<p className="name">
+							@2022 -- OlakunleBalo -- Being and Building Software <br />{" "}
 							Applications since 2019
 						</p>
 						<p>SiteMap | Terms | Privacy | Cookies | Advertisement</p>
 					</div>
-					<div className="second">
-						<FaTwitter />
-						<FaGithub />
-						<FaLinkedin />
-						<FaYoutube />
-						<FaInstagram />
-						<FaFacebookF />
+					<div className="second d-flex flex-column">
+						<IconContext.Provider
+							value={{
+								color: "white",
+								size: "1.5em",
+								className: "global-class-name",
+							}}
+						>
+							<div className="one d-flex">
+								<FaTwitter className="flex-fill" />
+								<FaGithub className="flex-fill" />
+								<FaLinkedin className="flex-fill" />
+							</div>
+							<div className="two d-flex">
+								<FaYoutube className="flex-fill" />
+								<FaInstagram className="flex-fill" />
+								<FaFacebookF className="flex-fill" />
+							</div>
+						</IconContext.Provider>
 					</div>
 				</div>
 			</div>
